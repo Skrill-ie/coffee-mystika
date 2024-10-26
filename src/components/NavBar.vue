860dac9df27a9f796f95611657d665b1f1d6f850
Skrill-ie <workhagz98@gmail.com>
2024-10-27 04:50:30 +0800

    Wireframed nav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,16 +1,21 @@
 <template>
-   <nav>
-    <ul>
+   <nav class="flex justify-between container mx-auto">
+    <router-link to="/">Home</router-link>
+    <ul class="flex w-2/4 justify-between">
       <li>
-        <router-link to="/">Home</router-link>
+        <router-link to="/about">Classes</router-link>
       </li>
       <li>
         <router-link to="/about">About</router-link>
       </li>
       <li>
+        <router-link to="/contact">Blog</router-link>
+      </li>
+      <li>
         <router-link to="/contact">Contact</router-link>
       </li>
     </ul>
+    <router-link to="/">Shop</router-link>
   </nav>
 </template>
 

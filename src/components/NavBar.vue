04d4433cb2e168e596fe45d3ab843a173380d814
Skrill-ie <workhagz98@gmail.com>
2024-10-27 21:22:17 +0800

    nav bar design initial

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,22 +1,30 @@
 <template>
-   <nav class="flex justify-between container mx-auto">
-    <router-link to="/">Home</router-link>
-    <ul class="flex w-2/4 justify-between">
-      <li>
-        <router-link to="/about">Classes</router-link>
-      </li>
-      <li>
-        <router-link to="/about">About</router-link>
-      </li>
-      <li>
-        <router-link to="/contact">Blog</router-link>
-      </li>
-      <li>
-        <router-link to="/contact">Contact</router-link>
-      </li>
-    </ul>
-    <router-link to="/">Shop</router-link>
-  </nav>
+  <div class="">
+    <div class="container mx-auto flex justify-between py-2">
+      <p><span class="text-text-divider">Location :</span><span class="text-text-paragraph"> Valencia Negros Oriental</span></p>
+      <p><span class="text-text-divider">Opening Hours :</span><span class="text-text-paragraph"> 10 : 00 AM - 11 : 00 PM</span></p>
+    </div>
+    <nav class="py-10 bg-black-secondary bg-opacity-50  text-white font-radley">
+      <div class="container mx-auto flex justify-between">
+        <router-link to="/">Home</router-link>
+        <ul class="flex w-2/4 justify-between">
+          <li>
+            <router-link to="/about" class="px-5 py-3 w-32" exact-active-class="bg-button-active">Classes</router-link>
+          </li>
+          <li>
+            <router-link to="/about" class="px-5 py-3 w-32" exact-active-class="bg-button-active">About</router-link>
+          </li>
+          <li>
+            <router-link to="/contact" class="px-5 py-3 w-32" exact-active-class="bg-button-active">Blog</router-link>
+          </li>
+          <li>
+            <router-link to="/contact" class="px-5 py-3 w-32" exact-active-class="bg-button-active">Contact Us</router-link>
+          </li>
+        </ul>
+        <router-link to="/">Shop</router-link>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        black:{
+          primary: '#242424',
+          secondary:'#0B0B0B'
+        },
+        text:{
+          title:'#F9DBB7',
+          paragraph:'#FDECD8',
+          divider:'#BA7345'
+        }, 
+        button:{
+          active:"#C58244",
+        }
+      },
+      fontFamily:{
+        radley: ['Radley', 'serif'], // Custom font for Radley
+        imFell: ['Im Fell English', 'serif'], // Custom font for Im Fell
+      },
+    },
   },
   plugins: [],
 }

3bba859fa30e5b2b8c3b1be1ed7c6a66e97e0524
RayQui <quiray511@gmail.com>
2024-10-29 14:29:33 +0800

    Create Blog.vue

---
+++ src/components/Blog.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1>Blog</h1>
+      <p>This is the blog butol page.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'BlogPage',
+  };
+  </script>
+  

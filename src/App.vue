381b0c09b3b1b1cc4df097aa1020a5f95ade821e
Skrill-ie <workhagz98@gmail.com>
2024-10-29 13:53:45 +0800

    footer initial design

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,20 @@
   <NavAppBar/>
   <router-view/>
   <AppFooter/>
+  <div class="absolute bottom-0 right-0 w-24 h-24 m-10 bg-text-title rounded-full shine-effect"><SocialIcons/></div>
 </template>
 
 <script>
-// import { HelloWorld } from './router';
 import NavAppBar from './components/NavBar.vue'; // Import the Navbar component
 import AppFooter from './components/Footer.vue'; // Import the Footer component
+import SocialIcons from './components/SocialIcon.vue'; // Import the Footer component
  
 export default {
   name: 'App',
   components: {
     NavAppBar,
     AppFooter,
+    SocialIcons,
   }
 }
 </script>
@@ -27,4 +29,18 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+@keyframes shine {
+    0% {
+      background-position: -200%;
+    }
+    100% {
+      background-position: 200%;
+    }
+  }
+  .shine-effect {
+    background: linear-gradient(45deg, rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.2));
+    background-size: 200% 200%;
+    animation: shine 1.5s infinite;
+    
+  }
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="">
-    <div class="container mx-auto flex justify-between py-2">
-      <p><span class="text-text-divider">Location :</span><span class="text-text-paragraph"> Valencia Negros Oriental</span></p>
-      <p><span class="text-text-divider">Opening Hours :</span><span class="text-text-paragraph"> 10 : 00 AM - 11 : 00 PM</span></p>
+  <div>
+    <div class="container mx-auto flex justify-between py-2 text-sm">
+      <p class="flex"><img src="../assets/icons/Vector.png"><span class="text-text-divider">Location :</span><span class="text-text-paragraph"> Valencia Negros Oriental</span></p>
+      <p class="flex"><img src="../assets/icons/Vector-1.png"><span class="text-text-divider">Opening Hours :</span><span class="text-text-paragraph"> 10 : 00 AM - 11 : 00 PM</span></p>
     </div>
-    <nav class="py-6 bg-black-secondary bg-opacity-50  text-white font-radley">
+    <nav class="py-6 bg-black-secondary bg-opacity-65  text-white font-radley">
       <div class="container mx-auto flex justify-between">
-        <router-link to="/">Home</router-link>
+        <router-link to="/"><img src="../assets/icons/cafe-mystika-icon.png"></router-link>
         <ul class="grid grid-cols-4 gap-x-8 w-2/4 justify-between">
           <li class="flex-1">
             <router-link to="/about" class="px-5 py-3 w-full block rounded-lg transition ease-in-out delay-150" exact-active-class="bg-button-active">Classes</router-link>
@@ -21,7 +21,7 @@
             <router-link to="/contact" class="px-5 py-3 w-full block rounded-lg transition ease-in-out delay-150" exact-active-class="bg-button-active">Contact Us</router-link>
           </li>
         </ul>
-        <router-link to="/">Shop</router-link>
+        <router-link to="/"><img src="../assets/icons/shop-icon.png"></router-link>
       </div>
     </nav>
   </div>

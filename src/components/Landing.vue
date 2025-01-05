01bb8c8a51c189cbb3f8b41cc9fad5607e47fcc4
skrillie <workhagz98@gmail.com>
2025-01-05 20:22:47 +0800

    updated landing page

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,25 +1,27 @@
 <template>
-  <div class="h-screen"> <!--Hero Section-->
+  <section class="h-screen"> <!--Hero Section-->
     <div>
       <LandingHeroPage :heroImage="heroImage" :IconImage="IconImage" :mainText="mainText" :subText="subText"/>
     </div>
-  </div>
-  <div class="md:h-screen bg-black-secondary justify-items-center"><!--Third Section-->
-    <div class="grid grid-cols-2 gap-5 md:gap-10  max-w-screen-xl font-radley w-full m-auto items-center h-full">
-      <div class="col-span-2 md:col-span-1 h-52 md:h-4/5 bg-center bg-fixed lg:bg-local bg-no-repeat" :style="{ backgroundImage: `url(${require('../assets/images/landing/sec3image.png')})` }"></div>
-      <div class="col-span-2 md:col-span-1 text-left px-3 py-16 lg:py-0 mx-auto lg:w-96 ">
+  </section>
+  <section class="md:h-screen bg-black-secondary justify-items-center"><!--Third Section-->
+    <div class="flex flex-col md:flex-row md:gap-10 font-radley w-full m-auto items-center h-full">
+      <div class="w-full h-full md:w-1/2">
+        <img class="w-full h-80 md:h-full object-cover" src="../assets/images/landing/sec3image.png">
+      </div>
+      <div class="w-full md:w-1/2 text-left px-3 md:px-10 max-w-3xl py-16 lg:py-0">
         <div class="text-4xl md:text-5xl mb-16 text-text-title">Coffee and Science come together, warmly brewed.</div>
         <div class="text-text-paragraph text-sm">Lorem ipsum odor amet, consectetuer adipiscing elit. Interdum neque ultricies sem eros id? Consequat a curae quam faucibus mollis. Sodales tincidunt elementum cursus lobortis suspendisse aptent ligula.</div>
       </div>
     </div>
-  </div>
-  <div class="py-10 md:py-20">
-    <div class="text-text-title text-4xl md:text-5xl font-radley mb-10">Partnered Creations</div>
+  </section>
+  <section class="py-10 md:py-20">
+    <div class="text-text-title text-4xl md:text-5xl font-radley mb-16">Partnered Creations</div>
     <div class="block lg:hidden">
       <div class="flex w-full font-radley">
       <button
             v-for="(featuredMerch, index) in featuredMerch" :key="index" @click="setActiveTab(index)"
-            :class="['px-4 py-2 text-sm font-medium w-1/2',activeMerch === index ? 'rounded-t-lg bg-button-active text-black-secondary': 'bg-black-secondary']"
+            :class="['px-4 py-2 text-sm font-medium w-1/2',activeMerch === index ? 'rounded-t-lg bg-button-active text-black-secondary': 'bg-black-primary text-text-paragraph opacity-55']"
         >
           {{ featuredMerch.name }}
         </button>
@@ -29,10 +31,12 @@
             name="fade"
             mode="out-in"
         >
-        <div :key="activeMerch">
-          <img class="h-full w-full mx-auto" :src="featuredMerch[activeMerch].src">
-          <div class="pt-10 text-black-secondary">
-              <div class="text-4xl md:text-5xl pb-10 w-full"> {{ featuredMerch[activeMerch].name }}</div>
+        <div :key="activeMerch" class="block md:flex">
+          <div class="w-full md:w-1/2">
+            <img class="h-full w-full mx-auto" :src="featuredMerch[activeMerch].src">
+          </div>
+          <div class="w-full md:w-1/2 pt-10 md:pl-5 text-black-secondary">
+              <div class="text-4xl md:text-5xl pb-16 w-full"> {{ featuredMerch[activeMerch].name }}</div>
               <div class="">{{ featuredMerch[activeMerch].description }}</div>
               <div class="pt-10">
                 <div class="pb-3"> Available sizes: </div>
@@ -54,8 +58,10 @@
     </div>
     <div class="hidden lg:flex gap-16 font-radley container mx-auto">
       <div v-for="(items,index) in featuredMerch" :key="index">
-        <div class="flex bg-button-active">
-          <img class="w-1/2" :src="items.src">
+        <div class="flex lg:flex-col bg-button-active">
+          <div class="w-1/2 lg:w-3/4 mx-auto pt-10">
+            <img class="w-full" :src="items.src">
+          </div>
           <div class="py-10 text-black-secondary px-3 text-left">
             <div class="text-4xl md:text-5xl pb-10 w-full"> {{ items.name }}</div>
             <div class="">{{ items.description }}</div>
@@ -74,11 +80,10 @@
             <a target="_blank" href="https://m.me/CafeMystika" class="w-full bg-black-secondary text-text-paragraph block text-center py-5">Let's Connect!</a>
           </div>
         </div>
-
       </div>
     </div>
-  </div>
-  <div class="bg-black-secondary justify-items-center py-10 md:py-20"><!--Second Section-->
+  </section>
+  <section class="bg-black-secondary justify-items-center py-10 md:py-20"><!--Second Section-->
     <div class="font-radley px-3 pb-16 lg:pb-20 text-left md:text-center">
       <div class="text-4xl md:text-5xl text-text-title pb-10 w-full ">Our customer's favorite</div>
       <div class="text-sm text-text-paragraph max-w-3xl">Lorem ipsum odor amet, consectetuer adipiscing elit. Interdum neque ultricies sem eros id? Consequat a curae quam faucibus mollis. Sodales tincidunt elementum cursus lobortis suspendisse aptent ligula.</div>
@@ -90,9 +95,9 @@
         </div>
       </div>
     </div>
-  </div>
-  <div class="bg-black-secondary justify-items-center"><!--Fifth Section-->
-    <div class="flex flex-col font-radley w-full m-auto items-center max-w-screen-xl lg:py-20">
+  </section>
+  <section class="bg-black-secondary justify-items-center"><!--Fifth Section-->
+    <div class="flex flex-col lg:flex-row font-radley w-full m-auto items-center max-w-screen-xl lg:py-20">
       <div class="order-2 md:order-1 px-3 lg:px-10 py-10 lg:py-0 mx-auto text-left lg:mr-16">
         <div class="text-4xl md:text-5xl text-text-title">Simply Cozy Sips</div>
         <div class="text-sm  text-text-paragraph py-10 border-text-divider border-b">Lorem ipsum odor amet, consectetuer adipiscing elit. Interdum neque ultricies sem eros id?</div>
@@ -109,13 +114,13 @@
         </ul>
         </div>
       </div>
-      <div class="order-1 md:order-2 m-auto h-96 hidden lg:block">
+      <div class="order-1 md:order-2 m-auto hidden lg:block">
         <img class="w-full" src="../assets/images/landing/sec5image.png">
       </div>
     </div>
-  </div>
-  <div class="bg-black-primary justify-items-center py-10 lg:py-20"><!--Sixth Section-->
-    <h1 class="text-4xl md:text-5xl text-text-title pb-10 w-full font-radley">We work hand-in-hand<br>with the best people!</h1>
+  </section>
+  <section class="bg-black-primary justify-items-center py-10 lg:py-20"><!--Sixth Section-->
+    <h1 class="text-4xl md:text-5xl text-text-title pb-16 w-full font-radley">We work hand-in-hand<br>with the best people!</h1>
     <div class="w-full max-w-4xl">
       <ul class="grid lg:gap-80 grid-cols-2">
         <li>
@@ -130,9 +135,9 @@
         </li>
       </ul>
     </div>
-  </div>
-  <div class="h-full bg-black-secondary justify-items-center py-10 lg:py-20"><!--Seventh Section-->
-    <h1 class="text-4xl md:text-5xl text-text-title w-full font-radley pb-10">A taste of the custom drinks <br>we've crafted for you.</h1>
+  </section>
+  <section class="h-full bg-black-secondary justify-items-center py-10 lg:py-20"><!--Seventh Section-->
+    <h1 class="text-4xl md:text-5xl text-text-title w-full font-radley pb-16">A taste of the custom drinks <br>we've crafted for you.</h1>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 px-3 ">
       <div v-for="(video, index) in tutorialDrinks" :key="'video'+index" class="grid-cols-1">
         <iframe :src="video.link"
@@ -148,7 +153,7 @@
       </div>
     </div>
 
-  </div>
+  </section>
 </template>
 
 <script>

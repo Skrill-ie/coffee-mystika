4021315348379c09ac38096c39c15c43c7a83fa8
skrillie <37149186+skrillie@users.noreply.github.com>
2025-03-19 21:41:21 +0800

    Blog indeces update

--- src/components/sub-component/BlogModal.vue
+++ src/components/sub-component/BlogModal.vue
@@ -1,6 +1,6 @@
 <template>
-  <div v-if="showBlogModal" class="fixed inset-0 flex items-center justify-center z-50 font-radley overflow-y-auto">
-    <div class="bg-black-secondary w-full relative h-screen bg-opacity-50 flex">
+  <div v-if="showBlogModal" class="fixed inset-0 flex items-center justify-center z-50 font-radley ">
+    <div class="bg-black-secondary w-full relative h-screen bg-opacity-50 flex overflow-y-auto">
       <div class="bg-black-secondary rounded-lg shadow-lg max-w-3xl w-full z-10 m-auto relative">
         <div class="font-imFell backdrop-blur-md absolute h-80 w-full justify-center flex flex-col space-y-5 items-center px-5 md:px-10 z-20">
           <span class="text-5xl text-text-title">{{currentBlog.title}}</span>
@@ -10,7 +10,7 @@
         <div class="h-80 bg-cover bg-no-repeat bg-center" :style="{ backgroundImage: `url(${currentBlog.blogImage})`}"></div>
         
         <div class="px-5 lg:px-10 mt-20 pb-20">
-          <div class= "text-text-paragraph mb-20 text-left indent-8">{{ currentBlog.content }}</div>
+          <div v-html="currentBlog.content" class= "text-text-paragraph mb-20 text-left indent-8"></div>
           <img class="mb-20" v-bind="{src:currentBlog.blogImageModal}">
           <!--<div class= "mb-20"><img :scr="`require('@${currentBlog.blogImageModal}')`"></div>-->
           <!--<div class="h-[30rem] mb-20 bg-contain bg-no-repeat bg-center"></div>-->
